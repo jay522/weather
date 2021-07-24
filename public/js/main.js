3a5146e23efe64b4e032d8164069a8545d870e85
jay522 <jayrajverma7777@gmail.com>
2021-07-24 15:33:53 +0530

    css part

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,7 @@ const getInfo=async(event)=>{
         datahide.classList.add('data_hide');
     }else{
         try{
-            let url=`http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=968c773515c8b62bcd6c9b8feb4f67f4`
+            let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=968c773515c8b62bcd6c9b8feb4f67f4`
             const response=await fetch(url);
             const data=await response.json();
             // console.log(data);
